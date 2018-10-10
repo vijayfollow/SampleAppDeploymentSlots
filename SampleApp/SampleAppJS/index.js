9c20b9bcb0d89c76ede8858b74130e6258f4a528
Viji Mani <M1047737@mindtree.com>
2018-10-10 14:22:00 +0530

    changes Desc

---
+++ SampleApp/SampleAppJS/index.js
@@ -0,0 +1,17 @@
+﻿
+
+$(document).ready(function () {
+
+});
+
+function OpenDescription() {
+    $("#divDesc").css("display", "");
+    $("#pTagOpen").css("display", "none");
+    $("#pTagClose").css("display", "");
+}
+
+function HideDescription() {
+    $("#divDesc").css("display", "none");
+    $("#pTagOpen").css("display", "");
+    $("#pTagClose").css("display", "none");
+}
