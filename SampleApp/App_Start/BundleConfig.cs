c84d58b9614863b22950c751ac36f367526ea643
Viji Mani <M1047737@mindtree.com>
2018-10-11 11:40:37 +0530

    Bundleconfig url change

--- SampleApp/App_Start/BundleConfig.cs
+++ SampleApp/App_Start/BundleConfig.cs
@@ -8,19 +8,19 @@ namespace SampleApp
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("https://deploymentslotscdn.azureedge.net/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("https://deploymentslotscdn.azureedge.net/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("https://deploymentslotscdn.azureedge.net/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("https://deploymentslotscdn.azureedge.net/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
